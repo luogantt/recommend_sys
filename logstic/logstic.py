2b06685ee79a83e781fd30394b226675b1e0e374
luogantt <luogantt@github.com>
2019-04-24 12:02:03 +0800

    新文件：   Factorization_Machine/fm.gif 	新文件：   logstic/lf1000.gif 	新文件：   logstic/logstic.py 	新文件：   logstic/testSet.txt

---
+++ logstic/logstic.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 22 16:30:47 2019
+
+@author: luogantt
+"""
+
+'''
+Created on Oct 27, 2010
+Logistic Regression Working Module
+@author: Peter
+'''
+from numpy import *
+
+def loadDataSet():
+    dataMat = []; labelMat = []
+    fr = open('testSet.txt')
+    for line in fr.readlines():
+        lineArr = line.strip().split()
+        dataMat.append([1.0, float(lineArr[0]), float(lineArr[1])])
+        labelMat.append(int(lineArr[2]))
+    return dataMat,labelMat
+
+def sigmoid(inX):
+    return 1.0/(1+exp(-inX))
+
+def gradAscent(dataMatIn, classLabels):
+    dataMatrix = mat(dataMatIn)             #convert to NumPy matrix
+    labelMat = mat(classLabels).transpose() #convert to NumPy matrix
+    n,m = shape(dataMatrix)
+    alpha = 0.001
+    maxCycles = 5000
+    weights = ones((m,1))
+    for k in range(maxCycles):              #heavy on matrix operations
+        h = sigmoid(dataMatrix*weights)     #matrix mult
+        error = (labelMat - h)              #vector subtraction
+        weights = weights + alpha * dataMatrix.transpose()* error #matrix mult
+    return weights
+
+def plotBestFit(weights):
+    import matplotlib.pyplot as plt
+    dataMat,labelMat=loadDataSet()
+    dataArr = array(dataMat)
+    n = shape(dataArr)[0] 
+    xcord1 = []; ycord1 = []
+    xcord2 = []; ycord2 = []
+    for i in range(n):
+        if int(labelMat[i])== 1:
+            xcord1.append(dataArr[i,1]); ycord1.append(dataArr[i,2])
+        else:
+            xcord2.append(dataArr[i,1]); ycord2.append(dataArr[i,2])
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(xcord1, ycord1, s=30, c='red', marker='s')
+    ax.scatter(xcord2, ycord2, s=30, c='green')
+    x = arange(-3.0, 3.0, 0.1)
+    y = (-weights[0]-weights[1]*x)/weights[2]
+    ax.plot(x, y)
+    plt.xlabel('X1'); plt.ylabel('X2');
+    plt.show()
+
+#import logRegres    
+
+dataArr,labelMat=loadDataSet() 
+
+
+weights=gradAscent(dataArr,labelMat)
+plotBestFit(weights.getA())
